0bbb9f265d17dfe94f879ecf8e9873e2f4051fd8
spencerpomme <spencerpomme@live.com>
2015-05-10 00:14:06 +0800

    removed test code

--- sametree_20150509.py
+++ sametree_20150509.py
@@ -1,10 +1,3 @@
-class Treenode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
 class Solution:
     # @param p, a tree node
     # @param q, a tree node
@@ -19,24 +12,3 @@ class Solution:
         else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-if __name__ == '__main__':
-    p = Treenode(3)
-    p.left = Treenode(1)
-    p.right = Treenode(2)
-    p.left.left = Treenode(1)
-    p.left.right = Treenode(0)
-    p.right.left = Treenode(1)
-    p.right.right = Treenode (1)
-
-    q = Treenode(3)
-    q.left = Treenode(1)
-    q.right = Treenode(2)
-    q.left.left = Treenode(1)
-    q.left.right = Treenode(0)
-    q.right.left = Treenode(1)
-    q.right.right = Treenode (1)
-    test = Solution()
-    a = test.isSameTree(p,q)
-    print(a)
-    q = Treenode(1)
-    print(q)
