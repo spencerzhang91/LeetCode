ac8d020ff2cad2b933d7165b6bbfb66713d2f417
spencerpomme <spencerpomme@live.com>
2015-05-10 00:14:49 +0800

    a recursive way

--- maxium_depth_20150510.py
+++ maxium_depth_20150510.py
@@ -7,20 +7,3 @@ class Solution:
         else:
             return max(1 + self.maxDepth(root.left), 1 + self.maxDepth(root.right))
 
-class Treenode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-if __name__ == '__main__':
-    p = Treenode(3)
-    p.left = Treenode(1)
-    p.right = Treenode(2)
-    p.left.left = Treenode(1)
-    p.left.right = Treenode(0)
-    p.right.left = Treenode(1)
-    p.right.right = Treenode (1)
-
-    test = Solution()
-    print(test.maxDepth(p))
