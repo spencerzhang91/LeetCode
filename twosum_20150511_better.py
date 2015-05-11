38c64d22b868175691e379ba73ee24c676de5e36
spencerpomme <spencerpomme@live.com>
2015-05-11 12:30:50 +0800

    tiny modification

--- twosum_20150511_better.py
+++ twosum_20150511_better.py
@@ -1,15 +1,17 @@
 class Solution:
     def twoSum(self, num, target):
-        map = {}
+        map_n = {}
         for i in range(len(num)):
-            if num[i] not in map:
-                map[target - num[i]] = i
+            if num[i] not in map_n:
+                map_n[target - num[i]] = i
+                print(i, ':', num[i])
+                print(map_n)
             else:
-                return map[num[i]] + 1, i + 1
+                return map_n[num[i]] + 1, i + 1
         return -1, -1
 
 if __name__ == "__main__":
     
     test = Solution()
-    a = test.twoSum([1,2,3,11,22,33], 44)
+    a = test.twoSum([1,43], 44)
     print(a)
