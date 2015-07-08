3040ee9e75309833a618299f763c87b3e4866467
Spencer Zhang <spencerpomme@live.com>
2015-07-08 13:55:33 +0800

    ...

--- rotate_array_20150527.py
+++ rotate_array_20150527.py
@@ -7,14 +7,20 @@ class Solution:
             res = nums[-k%n:] + nums[:-k%n]
             for i in range(len(res)):
                 nums[i] = res[i]
-                
-        # return nums
+    
+    # Dino's second solution below
+    def rotate2(self, nums, k):
+        n = len(nums) - k
+        if n < 0:
+           n = len(nums)+n
+        print(n)
+        nums = nums[n:] + nums[:n]
 
 
     
 if __name__ == '__main__':
     test = Solution()
     nums = [1,2]
-    k = 1
+    k = -3
     print(test.rotate(nums, k))
     
