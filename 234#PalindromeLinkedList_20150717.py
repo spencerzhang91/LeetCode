6fa222f2d876283e2f7d248608000bef4a76af1f
Spencer Zhang <spencerpomme@live.com>
2015-07-17 19:50:53 +0800

    7/17

--- 234#PalindromeLinkedList_20150717.py
+++ 234#PalindromeLinkedList_20150717.py
@@ -1,3 +1,4 @@
+# initial approach
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -9,25 +10,37 @@ class ListNode:
 
 class Solution:
     # @param {ListNode} head
-    # @return {ListNode}
-    def reverseList(self, head):
+    # @return {boolean}
+    def isPalindrome(self, head):
+        first = []
+        new = []
+        p1 = head
+        while p1 != None:
+            first.append(p1.val)
+            p1 = p1.next
         new_head = None
         while head != None:
             temp = head
             head = head.next
             temp.next = new_head
             new_head = temp
-        return new_head
+        p2 = new_head
+        while p2 != None:
+            new.append(p2.val)
+            p2 = p2.next
+        
+        return first == new
+
 
 # test code
 if __name__ == '__main__':
     
     n0 = ListNode('a')
-    n1 = ListNode('b')
-    n2 = ListNode('c')
-    n3 = ListNode('d')
+    n1 = ListNode('a')
+    n2 = ListNode('a')
+    n3 = ListNode('a')
 
     n0.next = n1; n1.next = n2; n2.next = n3
     
     test = Solution()
-    print(test.reverseList(n0))
+    print(test.isPalindrome(n0))
