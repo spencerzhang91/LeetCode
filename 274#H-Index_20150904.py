fda471a5b3678654aa29484b799c388cb54445f1
Spencer Zhang <spencerpomme@live.com>
2015-09-04 22:43:19 +0800

    added a binary search algorithm

--- 274#H-Index_20150904.py
+++ 274#H-Index_20150904.py
@@ -1,4 +1,5 @@
 #274 H-Index
+# original approach: linear time
 class Solution(object):
     def hIndex(self, citations):
         """
@@ -33,6 +34,23 @@ class Solution:
             print(citations[i],"->",i+1)
             i += 1
         return i
+
+# binary search approach: O(log n) time
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        N = len(citations)
+        left, right = 0, N - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if N - mid > citations[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return N - left
     
 
 if __name__ == '__main__':
