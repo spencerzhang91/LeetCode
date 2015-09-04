1c8b4366527c365e8f35b767748da41ba09483e1
Spencer Zhang <spencerpomme@live.com>
2015-09-04 13:42:25 +0800

    ...

--- 274#H-Index_20150904.py
+++ 274#H-Index_20150904.py
@@ -4,22 +4,27 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        citations.sort()
-        h = 0
-        c = citations[0]
-        num = len(citations)
-        for i in range(1, num-1):
-            if citations[i] > c:
-                c = citations[i]
-                num -= 1
-                if num >= c:
-                    h = c
+        l = len(citations)
+        if l == 0:
+            return 0
+        if l == 1:
+            if citations[0] == 0:
+                return 0
             else:
-                h = c
+                return 1
+        citations.sort()
+        h = min(citations[0], l)
+        for i in range(l - 1):
+            fore_h = min(citations[i], l-i)
+            next_h = min(citations[i+1], l-(i+1))
+            if next_h >= fore_h:
+                h = next_h
+            else:
+                break
         return h
 
 if __name__ == '__main__':
-    c = [1,2,2,2]
+    c = [0,1,0]
     test = Solution()
     h = test.hIndex(c)
     print(h)
