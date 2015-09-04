264b7d92d2d591f75d9979c60929b1003493425a
Spencer Zhang <spencerpomme@live.com>
2015-09-04 22:33:20 +0800

    binary search to achieve O(log n) time complexity

---
+++ 275#H-Index2_20150904.py
@@ -0,0 +1,16 @@
+#275 H-index ii
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        N = len(citations)
+        left, right = 0, N - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if N - mid > citations[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return N - left
