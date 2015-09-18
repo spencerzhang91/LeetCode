cd32c389aa862247a4c43da09ca85cda464a093f
Spencer Zhang <spencerpomme@live.com>
2015-09-18 20:56:07 +0800

    one line

--- 88#MergeSorteArray_20150914.c
+++ 88#MergeSorteArray_20150914.c
@@ -1,5 +1,5 @@
 /* 88 Merge Sorted Array */
-
+// long stupid insert approach:
 void merge(int *nums1, int m, int *nums2, int n)
 {
 	
@@ -32,5 +32,13 @@ void insert_item(int num, int *arr, int len, int place)
 		arr[i+1] = arr[i];
 	}
 	arr[place] = num;
+}
+
+// and here comes the one line solution: 
+void merge(int* nums1, int m, int* nums2, int n) {
+    while(n)
+    {
+        nums1[m+n-1] = (m && nums1[m-1] > nums2[n-1]) ? nums1[--m]:nums2[--n];
+    }
 }
 
