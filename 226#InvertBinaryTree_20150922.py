68676e7aa1f11640a3f95bf3ca103815e9d7a2f9
Spencer Zhang <spencerpomme@live.com>
2015-09-22 09:33:05 +0800

    ...

---
+++ 226#InvertBinaryTree_20150922.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+        return root
+
