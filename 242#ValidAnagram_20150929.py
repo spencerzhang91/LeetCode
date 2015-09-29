1a5f348a12963d741ee1e901ce0b003f475d0ed8
Spencer Zhang <spencerpomme@live.com>
2015-09-29 12:08:19 +0800

    SEP29

---
+++ 242#ValidAnagram_20150929.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(list(s)) == sorted(list(t))
+
+    
+        
+        
