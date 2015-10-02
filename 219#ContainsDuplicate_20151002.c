8c091bcb97540912e041d40650f5c1bc90c30a9c
Spencer Zhang <spencerpomme@live.com>
2015-10-02 22:12:06 +0800

    OCT02

---
+++ 219#ContainsDuplicate_20151002.c
@@ -0,0 +1,12 @@
+/* #219 contains duplicate2 */
+/* basic slow unacceptable O(n^2) approach */
+
+bool containsNearbyDuplicate(int *nums, int numsSize, int k)
+{
+	for (int i = 0; i < numsSize; i++)
+		for (int j = i+1; j < i+k+1; j++)
+			if (nums[i] == nums[j])
+				return true;
+				
+	return false;
+}
