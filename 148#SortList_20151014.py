aca1447ecd38d60312bc47504ddcae47b4595587
Spencer Zhang <spencerpomme@live.com>
2015-10-15 00:08:11 +0800

    ...

---
+++ 148#SortList_20151014.py
@@ -0,0 +1,73 @@
+# 148 SortList -- O(n^2) time complexity insertion sort approach
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        return str(self.val)
+        
+
+class Solution:
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        temp = head
+        insert, bound, movenode = None, None, None
+        curr = head.next
+        while curr:                               # while first of unsorted nodes is not None
+            print('Oi:',insert, 'Ob:',bound, 'Om:',movenode, 'Oc:',curr, 'Oh:',head)
+            while temp != curr:                   # while last of sorted nodes doesn't overlap with curr
+                if temp.val <= curr.val:
+                    insert = temp
+                bound = temp
+                temp = temp.next
+            # 'bound.next is curr' at this point is implied
+            movenode = curr
+            curr = curr.next
+            
+            print('Ii:',insert, 'Ib:',bound, 'Im:',movenode, 'Ic:',curr, 'Ih:',head)
+            print(bound.next is movenode)
+            print(movenode.next is curr)
+            
+            if head.val > movenode.val:           # corner case1: all sorted nodes's value greater than movenode
+                print('first reached')
+                bound.next = movenode.next         # make link: bound->curr
+                movenode.next = head
+                head = movenode
+            elif insert is bound:                 # corner case2: curr shall stay where it was
+                print('second reached')
+                bound.next = movenode
+            else:                                 # standard situiation: TO BE DONE!!!!!!!
+                print('third reached')
+                movenode.next = insert.next
+                insert.next = movenode
+
+            temp = head                           # reset temp to head in next loop
+            traverse(head)
+            print('\n','--'*20)
+        return head
+
+def traverse(head):
+    curNode = head
+    while curNode != None:
+        print(curNode, end=' ')
+        curNode = curNode.next
+
+        
+if __name__ == "__main__":
+	node1 = ListNode(10)
+	node2 = ListNode(2)
+	node3 = ListNode(11)
+	node4 = ListNode(6)
+	node5 = ListNode(7)
+	node1.next = node2; node2.next = node3
+	node3.next = node4; node4.next = node5
+	# traverse(node1)
+	test = Solution()
+	test.sortList(node1)
+
+
