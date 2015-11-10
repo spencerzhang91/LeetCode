f2bc91a986733df46997d0ce0a7248792fa0df37
Spencer Zhang <spencerpomme@users.noreply.github.com>
2015-11-10 15:57:07 +0800

    NOV10

--- 147#InsertionSort_20151107.py
+++ 147#InsertionSort_20151107.py
@@ -20,8 +20,34 @@ def InsertionSort(array):
 ls = [100,20,30,8,3]
 print(ls)
 InsertionSort(ls)
+'''
+rule breaking shameless approach below:
+Final correct solution will be done within 20 minutes
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-                
+class Solution(object):
+    def insertionSortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        array = []
+        while head:
+            array.append(head.val)
+            head = head.next
+        array.sort()
+        curr = head = ListNode(0) # the temp head
+        for item in array:
+            temp = ListNode(item)
+            curr.next = temp
+            curr = curr.next
+        head = head.next
+        return head
             
                 
         
