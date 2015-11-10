857ddf4882b982fac821040be8f53f3b873855a2
Spencer Zhang <spencerpomme@users.noreply.github.com>
2015-11-10 16:00:06 +0800

    NOV10

--- 147#InsertionSort_20151107.py
+++ 147#InsertionSort_20151107.py
@@ -1,25 +1,5 @@
 #147 InsertionSort_20151107
 
-def InsertionSort(array):
-    '''
-    Array based implementation
-    '''
-    count = len(array)
-    for i in range(1, count):
-        key = array[i]
-        j = i - 1
-        while j >= 0:
-            if array[j] > key:
-                array[j+1] = array[j]
-                array[j] = key
-            j -= 1
-            print(array)
-    return array
-
-
-ls = [100,20,30,8,3]
-print(ls)
-InsertionSort(ls)
 '''
 rule breaking shameless approach below:
 Final correct solution will be done within 20 minutes
