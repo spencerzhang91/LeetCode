129fc1b9c867b3f07933240a9f209c230888abbd
Spencer Zhang <spencerpomme@users.noreply.github.com>
2015-11-10 16:17:23 +0800

    NOV10

--- 147#InsertionSort_20151107.py
+++ 147#InsertionSort_20151107.py
@@ -29,6 +29,30 @@ class Solution(object):
         head = head.next
         return head
             
-                
+            
+            
+class Solution:
+    # @param head, a ListNode
+    # @return a ListNode
+    def insertionSortList(self, head):
+        pre = cursor = dummy_head = ListNode(0)
+        dummy_head.next = head
+
+        while cursor.next:
+            if pre.next.val > cursor.next.val:
+                pre = dummy_head
+
+            while pre.next.val < cursor.next.val:
+                pre = pre.next
+
+            if pre != cursor:
+                node = cursor.next
+                cursor.next = node.next
+                node.next = pre.next
+                pre.next = node
+            else:
+                cursor = cursor.next
+
+        return dummy_head.next
         
     
