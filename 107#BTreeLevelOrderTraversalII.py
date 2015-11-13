e7f31a49d9d40d44c26da3b8586b87485390f9c4
Spencer Zhang <spencerpomme@live.com>
2015-11-13 20:21:36 +0800

    NOV13

--- 107#BTreeLevelOrderTraversalII.py
+++ 107#BTreeLevelOrderTraversalII.py
@@ -1,4 +1,4 @@
-# 102 Binary Tree Level Order Traversal
+# 107 Binary Tree Level Order Traversal II
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
@@ -8,8 +8,31 @@ class TreeNode(object):
     def __repr__(self):
         return str(self.val)
 
+def CreateBTree(infoset):
+    '''
+    Take a tuple contains binary tree structure information as
+    argument, construct tree recursively and return the root.
+    '''
+    # print('infoset:', infoset)
+    if infoset:
+        try:
+            root, leftsub, rightsub = infoset
+        except ValueError:
+            root = infoset[0]
+            leftsub = None
+            rightsub = None
+    else:
+        return None
+    root = TreeNode(root)
+    if not root:
+        return None
+    else:
+        root.left = CreateBTree(leftsub)
+        root.right = CreateBTree(rightsub)
+    return root
+
 class Solution(object):
-    def levelOrder(self, root):
+    def levelOrderBottom(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
@@ -31,8 +54,15 @@ class Solution(object):
             currvals = nextvals
             nextlevel = []
             nextvals = []
-        return result
+        return result[::-1]
 
+
+tree = ('a', ('b', ('c', 'e', ('f',None,('h','i',None))), ('d',None,'g')), None)
+testroot = CreateBTree(tree)
+print(testroot)
+test = Solution()
+res = test.levelOrder(testroot)
+print(res)
 
                             
         
