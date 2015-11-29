5832d9dc718932d6c535ca7cb875dc0b185d867e
Spencer Zhang <spencerpomme@live.com>
2015-11-29 21:16:12 +0800

    NOV29

---
+++ 112#pathsum_20151129.py
@@ -0,0 +1,15 @@
+# 112 path sum
+Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
