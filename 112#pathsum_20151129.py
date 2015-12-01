0da4af26bce2e015187d48076f0d23eceaa806d2
Spencer Zhang <spencerpomme@live.com>
2015-12-02 00:45:46 +0800

    DEC02
    iter and recur

--- 112#pathsum_20151129.py
+++ 112#pathsum_20151129.py
@@ -7,16 +7,35 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def hasPathSum(self, root, sum):
+    def hasPathSum1(self, root, s):
         """
         recursive approach
         """
         if not root:
             return False
         if not (root.left or root.right):
-            if root.val == sum:
+            if root.val == s:
                 return True
-        return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
+        return (self.hasPathSum(root.left, s-root.val) 
+                or self.hasPathSum(root.right, s-root.val))
+
+    def hasPathSum2(self, root, s):
+        """
+        iterative dfs approach
+        """
+        if not root:
+            return False
+        stack = [(root, root.val)]
+        while stack:
+            node, curs = stack.pop()
+            if not (node.left or node.right):
+                if curs == s:
+                    return True
+            if node.left:
+                stack.append((node.left, curs + node.left.val))
+            if node.right:
+                stack.append((node.right, curs + node.right.val))
+        return False
 
 
 if __name__ == "__main__":
@@ -25,5 +44,3 @@ if __name__ == "__main__":
     tree.left = TreeNode(3)
     tree.right = TreeNode(4)
     tree.left.right = TreeNode(20)
-
-    traversal(tree)
