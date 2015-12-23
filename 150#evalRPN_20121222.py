a3c42a213c7a003aacbc74df759657ebfb029cc8
Spencer Zhang <spencerpomme@live.com>
2015-12-23 12:47:03 +0800

    DEC23

--- 150#evalRPN_20121222.py
+++ 150#evalRPN_20121222.py
@@ -1,5 +1,4 @@
 # 150 Evaluate Reverse Polish Notation
-
 class Solution(object):
     def evalRPN(self, tokens):
         """
@@ -21,13 +20,43 @@ class Solution(object):
                     temp = left - right
                 elif token == '*':
                     temp = left * right
-                elif token == '/':
-                    try:
+                elif token == '/':            # notice: it's correct implementation in python 3
+                    try:                      # but leetcode judge is a little different
                         temp = left / right
                     except ZeroDivisionError:
                         return None
                 stack.append(temp)
         return stack[0]
+
+# leetcode passable version
+class Solution2(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        if not tokens: return None
+        left = right = None
+        stack = []
+        for token in tokens:
+            if token not in ['+', '-', '*', '/']:
+                stack.append(int(token))
+            else:
+                right = stack.pop()
+                left = stack.pop()
+                if token == '+':
+                    temp = left + right
+                elif token == '-':
+                    temp = left - right
+                elif token == '*':
+                    temp = left * right
+                elif token == '/':
+                    if left * right < 0 and left % right != 0:
+                        temp = left / right +1
+                    else:
+                        temp = left / right
+                stack.append(temp)
+        return stack.pop()
 
 if __name__ == '__main__':
     exp = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
