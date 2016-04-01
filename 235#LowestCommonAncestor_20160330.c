944344b3d3b181fe6e0b06583d744504c04e981d
Spencer Zhang <spencerpomme@live.com>
2016-04-01 08:00:57 +0800

    april fool

--- 235#LowestCommonAncestor_20160330.c
+++ 235#LowestCommonAncestor_20160330.c
@@ -9,5 +9,27 @@ struct TreeNode {
 
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q)
 {
-     
+     while (root)
+     {
+        switch (helper(root->val, p->val, q->val))
+        {
+            case -1: root = root->left;
+                     lowestCommonAncestor(root, p, q);
+                     break;
+            case  1: root = root->right;
+                     lowestCommonAncestor(root, p, q);
+                     break;
+            case  0: return root;
+        }
+     }
+     return NULL;
+}
+
+int helper(int pivot, int a, int b)
+{
+    if (a < pivot && b < pivot)
+        return -1;
+    else if (a > pivot && b > pivot)
+        return 1;
+    else return 0;
 }
