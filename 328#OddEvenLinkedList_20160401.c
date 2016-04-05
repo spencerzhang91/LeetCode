4eb85f0639de9a58f0223990a51889ed2e0a93bc
GHYJ-T440-01 <GHYJ-T440-01>
2016-04-05 18:24:26 +0800

    APL05

--- 328#OddEvenLinkedList_20160401.c
+++ 328#OddEvenLinkedList_20160401.c
@@ -1,17 +1,28 @@
 // 328 Odd Even Linked List
-
 // Definition for singly-linked list.
+#include <stdio.h>
+#include <stdlib.h>
+
 struct ListNode {
     int val;
     struct ListNode *next;
 };
 
+struct ListNode *oddEvenList(struct ListNode *head);
+void displayList(struct ListNode *head);
+struct ListNode *createList(int node_num);
+
 int main(void)
 {
-    /* --- */
+    /* This main function is for testing. */
+    struct ListNode *list;
+    list = createList(10);
+    displayList(list);
+
+    return 0;
 }
 
-struct ListNode* oddEvenList(struct ListNode* head)
+struct ListNode *oddEvenList(struct ListNode *head)
 {
     struct ListNode *Ohead, *Otracker = head;
     struct ListNode *Ehead, *Etracker = head->next;
@@ -37,5 +48,16 @@ void displayList(struct ListNode *head)
 
 struct ListNode *createList(int node_num)
 {
-    /* --- */
+    /* This function creates linked list with given nodes and acsending val */
+    struct ListNode *head = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head->val = 1; head->next = NULL;
+    struct ListNode *curr = head;
+    for (int i = 1; i < node_num; i++)
+    {
+        struct ListNode *temp = (struct ListNode *)malloc(sizeof(struct ListNode));
+        temp->val = 1 + i; temp->next = NULL;
+        curr->next = temp;
+        curr = curr->next;
+    }
+    return head;
 }
