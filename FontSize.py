6b4498e2f36b27c66839a8f5d868cba29cb30f5a
spencerpomme <spencerpomme@live.com>
2016-04-06 20:11:14 +0800

    ...

---
+++ FontSize.py
@@ -0,0 +1,19 @@
+# Font size problem of Microsoft interview
+testCaseNum = int(raw_input())
+for i in range(testCaseNum):
+	NPWHlist = raw_input().split(' ')
+	wordList = raw_input().split(' ')
+
+	pagragraghNum = int(NPWHlist[0])
+	pageNum = int(NPWHlist[1])
+	screenWidth = int(NPWHlist[2])
+	screenHeight = int(NPWHlist[3])
+	wordOfParagragh = [0] * pagragraghNum
+	for i in range(pagragraghNum):
+		wordOfParagragh[i] = int(wordList[i])
+
+	totalWords = sum(wordOfParagragh)
+	font = int((totalWords / (screenWidth * screenHeight)) ** 0.5)
+	print font
+
+
