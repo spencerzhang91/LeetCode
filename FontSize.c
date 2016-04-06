2b07763ec88d6b1a3ca152de39100b57331b5aff
spencerpomme <spencerpomme@live.com>
2016-04-06 19:52:13 +0800

    temp

---
+++ FontSize.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_PARAGRAGH 1000
+#define MAX_DIGIT 4
+
+int main(void)
+{
+	int testCaseNum, totalWords;
+	int screenWidth, screenHeight, paragraghNum, pageNum;
+	int wordsOfParagragh[MAX_PARAGRAGH];
+	scanf("%d", &testCaseNum);
+	for (int i = 0; i < testCaseNum; i++)
+	{
+		scanf("%d %d %d %d", &paragraghNum, &pageNum, &screenWidth, &screenHeight);
+		for (int p = 0; p < paragraghNum; p++)
+
+	}
+}
