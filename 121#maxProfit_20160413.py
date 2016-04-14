f5bb18801b7f97f9b115c4b8e279a1840efd3d1b
spencerpomme <spencerpomme@live.com>
2016-04-14 16:56:36 +0800

    APL14

--- 121#maxProfit_20160413.py
+++ 121#maxProfit_20160413.py
@@ -8,5 +8,16 @@ class Solution:
         :rtype: int
         """
         profit = 0
-        currentMinPrice = prices[0] if len(prices) > 0
-        
+        currentMinPrice = prices[0] if len(prices) > 0 else 0
+        for i in range(1, len(prices)):
+            currentMinPrice = min(currentMinPrice, prices[i])
+            profit = max(profit, prices[i] - currentMinPrice)
+            print("currentmin:", currentMinPrice, "profit:", profit)
+        return profit
+
+if __name__ == "__main__":
+
+    test = Solution()
+    prices = [1,2,3,4,5,10,20,40,3,2,1,20,24,100,2]
+    profit = test.maxProfit(prices)
+    print(profit)
