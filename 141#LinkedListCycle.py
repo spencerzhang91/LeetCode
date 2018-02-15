c0fb433352b5cb2c29c542a5c0770302bb5dfc16
スペンサー <spencerpomme@live.com>
2018-02-15 13:05:37 +0800

    141 LinkedListCycle

---
+++ 141#LinkedListCycle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 141 Linked List Cycle
+"""
+Created on Thu Feb 15 11:44:02 2018
+
+@author: spencer
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
