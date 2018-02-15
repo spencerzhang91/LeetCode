7b2ab01f1ddabcee1ff43600ea222fe1302be0f6
スペンサー <spencerpomme@live.com>
2018-02-15 17:15:38 +0800

    modify

--- 344#ReverseString.scala
+++ 344#ReverseString.scala
@@ -1,14 +1,5 @@
 // 344 Reverse String
 
 object Solution {
-    def reverseString(s: String): String = {
-        val len: Int = s.size
-        var k = len - 1
-        val arr = new Array[Char](len)
-        for (i <- 0 until len) {
-            arr(k) = s(i)
-            k -= 1
-        }
-        arr.mkString
-    }
+    def reverseString(s: String): String = s.reverse
 }
