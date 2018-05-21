2302787a5eaa388958a6a8abdfbfe629679444a7
spencerpomme <spencerpomme@live.com>
2018-05-21 10:30:24 +0800

    update format using gofmt

--- 830#Positions_of_Large_Groups.go
+++ 830#Positions_of_Large_Groups.go
@@ -1,33 +1,34 @@
+// 830. Positions of Large Groups
+
 package main
 
 import "fmt"
 
 func largeGroupPositions(S string) [][]int {
-    if len(S) < 3 {
-        return make([][]int, 0)
-    }
-    res := [][]int{}
-    i := 0
-    for i < len(S) - 1 {
-        count := 1
-        j := i + 1
-        for (j <= len(S)-1) && (S[j] == S[i]) {
-            j += 1
-            count += 1
-        }
-        if count >= 3 {
-            pair := []int {i, j-1}
-            res = append(res, pair)
-        }
-        i = j
-    }
-    return res
+	if len(S) < 3 {
+		return make([][]int, 0)
+	}
+	res := [][]int{}
+	i := 0
+	for i < len(S)-1 {
+		count := 1
+		j := i + 1
+		for (j <= len(S)-1) && (S[j] == S[i]) {
+			j += 1
+			count += 1
+		}
+		if count >= 3 {
+			pair := []int{i, j - 1}
+			res = append(res, pair)
+		}
+		i = j
+	}
+	return res
 }
 
-
 func main() {
-    str := "abbxxxxzzy"
-    res := largeGroupPositions(str)
-    fmt.Println(str)
-    fmt.Println(res)
+	str := "abbxxxxzzy"
+	res := largeGroupPositions(str)
+	fmt.Println(str)
+	fmt.Println(res)
 }
