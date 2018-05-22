ebda15a939ee08338ce25836d90e86428d014a07
spencerpomme <spencerpomme@live.com>
2018-05-23 03:03:32 +0800

    medium

--- 817#LinkedListComponents.py
+++ 817#LinkedListComponents.py
@@ -1,9 +1,9 @@
 # 817. Linked List Components
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def numComponents(self, head, G):
