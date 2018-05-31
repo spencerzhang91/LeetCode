aba353786025622c5c992664f6db5273207f5f7a
スペンサー <spencerpomme@live.com>
2018-05-31 17:55:15 +0800

    medium

--- 647#PalindromicSubstrings.py
+++ 647#PalindromicSubstrings.py
@@ -10,7 +10,7 @@ class Solution:
         for center in range(2*N - 1):
             left = center // 2
             right = left + center % 2
-            while left >= 0 and right < N and S[left] == S[irght]:
+            while left >= 0 and right < N and s[left] == s[right]:
                 res += 1
                 left -= 1
                 right += 1
