1d5e753a2e1488a718eafb7a6656a180ee253cf5
spencerpomme <spencerpomme@live.com>
2018-06-05 12:16:10 +0800

    easy

---
+++ 198#HouseRobber.go
@@ -0,0 +1,25 @@
+// 198. House Robber
+func rob(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    if len(nums) < 2 {
+        return nums[0]
+    }
+    pprev := nums[0]
+    prev := max(nums[0], nums[1])
+    for i := 2; i < len(nums); i++ {
+        curr := max(pprev + nums[i], prev)
+        pprev = prev
+        prev = curr
+    }
+    return max(pprev, prev)
+}
+
+func max(a int, b int) int {
+    if a < b {
+        return b
+    } else {
+        return a
+    }
+}
