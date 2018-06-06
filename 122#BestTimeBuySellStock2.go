b187f60df95cc4894c1fed0c801616073f894f39
spencerpomme <spencerpomme@live.com>
2018-06-06 18:55:01 +0800

    medium

--- 122#BestTimeBuySellStock2.go
+++ 122#BestTimeBuySellStock2.go
@@ -1,8 +1,7 @@
-// 714. Best Time to Buy and Sell Stock with Transaction Fee
-package main
-import "fmt"
+// 122. Best Time to Buy and Sell Stock II
+package 
 
-func maxProfit(prices []int, fee int) int {
+func maxProfit(prices []int) int {
     ds := len(prices)
     if ds <= 1 {
         return 0
@@ -12,7 +11,7 @@ func maxProfit(prices []int, fee int) int {
     s0[0] = 0                // The max profit here is when no transaction is made
     s1[0] = -prices[0]       // State 1 initial profit
     for i := 1; i < ds; i++ {
-        s0[i] = max(s0[i-1], s1[i-1]+prices[i]-fee) // max(rest, sell)
+        s0[i] = max(s0[i-1], s1[i-1]+prices[i]) // max(rest, sell)
         s1[i] = max(s1[i-1], s0[i-1]-prices[i]) // max(rest, buy)
         // buy and sell one stock only pay once!
     }
@@ -25,10 +24,3 @@ func max(a int, b int) int {
     }
     return a
 }
-
-func main() {
-    prices := []int {1, 3, 2, 8, 4, 9}
-    fee := 2
-    fmt.Println(maxProfit(prices, fee))
-}
-
