69417147ac55c478af6f5d92e60918c473515f88
スペンサー <spencerpomme@live.com>
2019-09-20 16:47:49 +0800

    Update 590#N-aryTreePostorderTraversal.py

--- 590#N-aryTreePostorderTraversal.py
+++ 590#N-aryTreePostorderTraversal.py
@@ -17,3 +17,26 @@ class Solution:
         self.res = []
         traverse(root)
         return self.res
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution2:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            for child in node.children:
+                stack.append(child)
+        return res[::-1]
